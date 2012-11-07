3219614916eb75cc37ada21688c4ddd6cac02ad6
max <max@endofinternet.hu>
2012-11-07 20:22:31 +0100

    lib.py

--- lib.py
+++ lib.py
@@ -4,18 +4,19 @@
 import sys
 
 def record():
-    bookname = input('What is your name: ')
-    print('This is what you have just typed in: {0}'.format(bookname))
+    r = input('Record: ')
+    print('This is what you have just typed in: {0}'.format(r))
 
     f = open('text.txt', 'a')
-    f.write(bookname)
-    f.write('\n')
+    f.write(r + '\n')
 
 def read():
     f = open('text.txt', 'r')
+    i = 0
     while True:
+        i += 1
         line = f.readline()
-        print(line)
+        print('[{0}] - {1}'.format(i,line))
         if len(line) == 0:
             break
 
@@ -24,7 +25,6 @@ def search():
     needle = input('Enter the name you\'re looking for: ')
     while True:
         line = f.readline()
-#        if needle in line:
         if line.find(needle) != -1:
             print('Yes it\'s here!')
             print('Here it is: {0}'.format(line))
@@ -37,7 +37,7 @@ def quit():
     sys.exit()
 
 print('What would you like to do?\n')
-choice = int(input('[1] Record a text\n[2] Read the file back\n[3] search\n[4] quit\n'))
+choice = int(input('[1] Record a text\n[2] Read the file back\n[3] Search\n[4] quit\n'))
 
 if choice == 1:
     record()
